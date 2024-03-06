bf50d80ae9aca24f157409bfba4ea8a56651cb56
zmdr0010 <89996377+zmdr0010@users.noreply.github.com>
2024-03-06 15:13:25 +0900

    Add files via upload

---
+++ test-draw-rect-box/drwInfo-sample-00.js
@@ -0,0 +1,532 @@
+const drwInfoSample00 = {
+  list: [
+    {
+      name: "unit-00",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 0,
+          x : 5,
+          y : 0
+        },
+        {
+          target: 4,
+          x : 6,
+          y : 7
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 9
+        },
+        {
+          target: 9,
+          x : 13,
+          y : 9
+        },
+        {
+          target: 10,
+          x : 6,
+          y : 14
+        }
+      ]
+    },
+    {
+      name: "unit-01",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 1,
+          x : 7,
+          y : 0
+        },
+        {
+          target: 4,
+          x : 6,
+          y : 6
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 8
+        },
+        {
+          target: 9,
+          x : 13,
+          y : 8
+        },
+        {
+          target: 10,
+          x : 6,
+          y : 13
+        }
+      ]
+    },
+    {
+      name: "unit-02",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 2,
+          x : 7,
+          y : 0
+        },
+        {
+          target: 4,
+          x : 6,
+          y : 5
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 7
+        },
+        {
+          target: 9,
+          x : 13,
+          y : 7
+        },
+        {
+          target: 10,
+          x : 6,
+          y : 12
+        }
+      ]
+    },
+    {
+      name: "unit-03",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 3,
+          x : 6,
+          y : 0
+        },
+        {
+          target: 4,
+          x : 6,
+          y : 9
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 11
+        },
+        {
+          target: 9,
+          x : 13,
+          y : 11
+        },
+        {
+          target: 10,
+          x : 6,
+          y : 16
+        }
+      ]
+    },
+    {
+      name: "unit-04",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 0,
+          x : 6,
+          y : 0
+        },
+        {
+          target: 5,
+          x : 6,
+          y : 9
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 12
+        },
+        {
+          target: 9,
+          x : 15,
+          y : 12
+        },
+        {
+          target: 10,
+          x : 7,
+          y : 18
+        }
+      ]
+    },
+    {
+      name: "unit-05",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 1,
+          x : 8,
+          y : 0
+        },
+        {
+          target: 5,
+          x : 6,
+          y : 8
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 11
+        },
+        {
+          target: 9,
+          x : 15,
+          y : 11
+        },
+        {
+          target: 10,
+          x : 7,
+          y : 17
+        }
+      ]
+    },
+    {
+      name: "unit-06",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 2,
+          x : 8,
+          y : 0
+        },
+        {
+          target: 5,
+          x : 6,
+          y : 6
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 9
+        },
+        {
+          target: 9,
+          x : 15,
+          y : 9
+        },
+        {
+          target: 10,
+          x : 7,
+          y : 15
+        }
+      ]
+    },
+    {
+      name: "unit-07",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 3,
+          x : 7,
+          y : 0
+        },
+        {
+          target: 5,
+          x : 6,
+          y : 10
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 13
+        },
+        {
+          target: 9,
+          x : 15,
+          y : 13
+        },
+        {
+          target: 10,
+          x : 7,
+          y : 19
+        }
+      ]
+    },
+    {
+      name: "unit-08",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 0,
+          x : 4,
+          y : 0
+        },
+        {
+          target: 6,
+          x : 6,
+          y : 9
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 10
+        },
+        {
+          target: 9,
+          x : 11,
+          y : 10
+        },
+        {
+          target: 10,
+          x : 5,
+          y : 16
+        }
+      ]
+    },
+    {
+      name: "unit-09",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 1,
+          x : 6,
+          y : 0
+        },
+        {
+          target: 6,
+          x : 6,
+          y : 8
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 9
+        },
+        {
+          target: 9,
+          x : 11,
+          y : 9
+        },
+        {
+          target: 10,
+          x : 5,
+          y : 15
+        }
+      ]
+    },
+    {
+      name: "unit-10",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 2,
+          x : 6,
+          y : 0
+        },
+        {
+          target: 6,
+          x : 6,
+          y : 6
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 7
+        },
+        {
+          target: 9,
+          x : 11,
+          y : 7
+        },
+        {
+          target: 10,
+          x : 5,
+          y : 13
+        }
+      ]
+    },
+    {
+      name: "unit-11",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 3,
+          x : 5,
+          y : 0
+        },
+        {
+          target: 6,
+          x : 6,
+          y : 10
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 11
+        },
+        {
+          target: 9,
+          x : 11,
+          y : 11
+        },
+        {
+          target: 10,
+          x : 5,
+          y : 17
+        }
+      ]
+    },
+    {
+      name: "unit-12",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 0,
+          x : 3,
+          y : 0
+        },
+        {
+          target: 7,
+          x : 5,
+          y : 9
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 11
+        },
+        {
+          target: 9,
+          x : 9,
+          y : 11
+        },
+        {
+          target: 10,
+          x : 4,
+          y : 19
+        }
+      ]
+    },
+    {
+      name: "unit-13",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 1,
+          x : 5,
+          y : 0
+        },
+        {
+          target: 7,
+          x : 5,
+          y : 8
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 10
+        },
+        {
+          target: 9,
+          x : 9,
+          y : 10
+        },
+        {
+          target: 10,
+          x : 4,
+          y : 18
+        }
+      ]
+    },
+    {
+      name: "unit-14",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 2,
+          x : 5,
+          y : 0
+        },
+        {
+          target: 7,
+          x : 5,
+          y : 6
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 8
+        },
+        {
+          target: 9,
+          x : 9,
+          y : 8
+        },
+        {
+          target: 10,
+          x : 4,
+          y : 16
+        }
+      ]
+    },
+    {
+      name: "unit-15",
+      size: 15,
+      x: 0,
+      y: 0,
+      list: [
+        {
+          target: 3,
+          x : 4,
+          y : 0
+        },
+        {
+          target: 7,
+          x : 5,
+          y : 10
+        },
+        {
+          target: 8,
+          x : 0,
+          y: 12
+        },
+        {
+          target: 9,
+          x : 9,
+          y : 12
+        },
+        {
+          target: 10,
+          x : 4,
+          y : 20
+        }
+      ]
+    }
+  ]
+}
---
+++ test-draw-rect-box/rbxInfo-sample-00.js
@@ -0,0 +1,215 @@
+// rect box info sample 00
+
+const frontSample00 = {
+  type: "f", // f(front), bd(body), l(left), r(right), bk(back),
+  name: "front-sample-00",
+  size: { w:9, h:9 },
+  // color: "black", // rgba(0, 0, 200, 0.5) // rgb(200, 0, 0)
+  child: [
+    {
+      // name: "front-sample-00-sub-00",
+      color: "rgb(50, 50, 50)",
+      rbxs: [
+        { x:2, y:0 }, { x:6, y:0 }, { x:2, y:1 }, { x:6, y:1 },
+        { x:1, y:2 }, { x:2, y:2 }, { x:6, y:2 }, { x:7, y:2 },
+        { x:0, y:3 }, { x:1, y:3 }, { x:2, y:3 }, { x:3, y:3 },
+        { x:4, y:3 }, { x:5, y:3 }, { x:6, y:3 }, { x:7, y:3 },
+        { x:8, y:3 }, { x:1, y:4 }, { x:2, y:4 }, { x:3, y:4 },
+        { x:4, y:4 }, { x:5, y:4 }, { x:6, y:4 }, { x:7, y:4 },
+        { x:2, y:5 }, { x:3, y:5 }, { x:4, y:5 }, { x:5, y:5 },
+        { x:6, y:5 }, { x:2, y:6 }, { x:3, y:6 }, { x:4, y:6 },
+        { x:5, y:6 }, { x:6, y:6 }, { x:2, y:7 }, { x:3, y:7 },
+        { x:4, y:7 }, { x:5, y:7 }, { x:6, y:7 }, { x:3, y:8 },
+        { x:4, y:8 }, { x:5, y:8 }
+      ]
+    },
+    {
+      color: "rgb(70, 70, 70)",
+      rbxs: [
+        { x:0, y:4 }, { x:1, y:4 }, { x:2, y:4 }, { x:6, y:4 },
+        { x:7, y:4 }, { x:8, y:4 }, { x:1, y:5 }, { x:2, y:5 },
+        { x:6, y:5 }, { x:7, y:5 }, { x:2, y:6 }, { x:3, y:6 },
+        { x:5, y:6 }, { x:6, y:6 }, { x:3, y:7 }, { x:4, y:7 },
+        { x:5, y:7 }
+      ]
+    },
+    {
+      color: "rgb(90, 90, 90)",
+      rbxs: [
+        { x:3, y:1 }, { x:4, y:1 }, { x:5, y:1 }, { x:3, y:2 },
+        { x:4, y:2 }, { x:5, y:2 }, { x:3, y:3 }, { x:4, y:3 },
+        { x:5, y:3 }, { x:3, y:4 }, { x:4, y:4 }, { x:5, y:4 }
+      ]
+    },
+    {
+      color: "rgb(150, 150, 150)",
+      rbxs: [
+        { x:4, y:1 }, { x:3, y:2 }, { x:4, y:2 }, { x:5, y:2 },
+        { x:4, y:3 }, { x:4, y:4 }
+      ]
+    }
+  ]
+}
+
+const frontSample01 = {
+  type: "f", // f(front), bd(body), l(left), r(right), bk(back),
+  name: "front-sample-01",
+  size: { w:5, h:8 },
+  // color: "black", // rgba(0, 0, 200, 0.5) // rgb(200, 0, 0)
+  child: [
+    {
+      // name: "front-sample-00-sub-00",
+      color: "rgb(50, 50, 50)",
+      rbxs: [
+        { x:2, y:1 }, { x:1, y:2 }, { x:2, y:2 }, { x:3, y:2 },
+        { x:0, y:3 }, { x:1, y:3 }, { x:2, y:3 }, { x:3, y:3 },
+        { x:4, y:3 }
+      ]
+    },
+    {
+      color: "rgb(70, 70, 70)",
+      rbxs: [
+        { x:2, y:1 }, { x:1, y:2 }, { x:2, y:2 }, { x:3, y:2 },
+        { x:1, y:3 }, { x:2, y:3 }, { x:3, y:3 }, { x:0, y:4 },
+        { x:1, y:4 }, { x:2, y:4 }, { x:3, y:4 }, { x:4, y:4 },
+        { x:1, y:5 }, { x:2, y:5 }, { x:3, y:5 }, { x:1, y:6 },
+        { x:2, y:6 }, { x:3, y:6 }, { x:1, y:7 }, { x:2, y:7 },
+        { x:3, y:7 }
+      ]
+    },
+    {
+      color: "rgb(90, 90, 90)",
+      rbxs: [
+        { x:0, y:0 }, { x:4, y:0 }, { x:0, y:1 }, { x:1, y:1 },
+        { x:3, y:1 }, { x:4, y:1 }, { x:1, y:2 }, { x:2, y:2 },
+        { x:3, y:2 }
+      ]
+    },
+    {
+      color: "rgb(150, 150, 150)",
+      rbxs: [
+        { x:1, y:4 }, { x:3, y:4 }, { x:1, y:5 }, { x:2, y:5 },
+        { x:3, y:5 }
+      ]
+    },
+    {
+      color: "rgb(160, 160, 160)",
+      rbxs: [
+        { x:2, y:5 }, { x:1, y:6 }, { x:2, y:6 }, { x:3, y:6 }
+      ]
+    },
+    {
+      color: "rgb(170, 170, 170)",
+      rbxs: [
+        { x:2, y:6 }, { x:2, y:7 }
+      ]
+    },
+    {
+      color: "rgb(180, 180, 180)",
+      rbxs: [
+        { x:2, y:2 }, { x:2, y:3 }
+      ]
+    }
+  ]
+}
+
+const frontSample02 = {
+  type: "f", // f(front), bd(body), l(left), r(right), bk(back),
+  name: "front-sample-02",
+  size: { w:5, h:8 },
+  // color: "black", // rgba(0, 0, 200, 0.5) // rgb(200, 0, 0)
+  child: [
+    {
+      // name: "front-sample-00-sub-00",
+      color: "rgb(50, 50, 50)",
+      rbxs: [
+        { x:1, y:0 }, { x:3, y:0 }, { x:0, y:1 }, { x:1, y:1 },
+        { x:2, y:1 }, { x:3, y:1 }, { x:4, y:1 }, { x:1, y:2 },
+        { x:2, y:2 }, { x:3, y:2 }, { x:1, y:3 }, { x:2, y:3 },
+        { x:3, y:3 }
+      ]
+    },
+    {
+      color: "rgb(70, 70, 70)",
+      rbxs: [
+        { x:1, y:5 }, { x:2, y:5 }, { x:3, y:5 }, { x:2, y:6 }
+      ]
+    },
+    {
+      color: "rgb(90, 90, 90)",
+      rbxs: [
+        { x:2, y:1 }, { x:2, y:2 }, { x:2, y:3 }, { x:1, y:4 },
+        { x:2, y:4 }, { x:3, y:4 }, { x:2, y:5 }
+      ]
+    },
+    {
+      color: "rgb(150, 150, 150)",
+      rbxs: [
+        { x:2, y:2 }, { x:2, y:3 }
+      ]
+    }
+  ]
+}
+
+const frontSample03 = {
+  type: "f", // f(front), bd(body), l(left), r(right), bk(back),
+  name: "front-sample-03",
+  size: { w:7, h:11 },
+  // color: "black", // rgba(0, 0, 200, 0.5) // rgb(200, 0, 0)
+  child: [
+    {
+      // name: "front-sample-00-sub-00",
+      color: "rgb(50, 50, 50)",
+      rbxs: [
+        { x:1, y:0 }, { x:5, y:0 }, { x:1, y:1 }, { x:5, y:1 },
+        { x:1, y:2 }, { x:2, y:2 }, { x:3, y:2 }, { x:4, y:2 },
+        { x:5, y:2 }, { x:1, y:3 }, { x:2, y:3 }, { x:3, y:3 },
+        { x:4, y:3 }, { x:5, y:3 }, { x:1, y:4 }, { x:2, y:4 },
+        { x:3, y:4 }, { x:4, y:4 }, { x:5, y:4 }, { x:0, y:5 },
+        { x:1, y:5 }, { x:2, y:5 }, { x:3, y:5 }, { x:4, y:5 },
+        { x:5, y:5 }, { x:6, y:5 }, { x:1, y:6 }, { x:2, y:6 },
+        { x:3, y:6 }, { x:4, y:6 }, { x:5, y:6 }, { x:2, y:7 },
+        { x:3, y:7 }, { x:4, y:7 }, { x:2, y:8 }, { x:3, y:8 },
+        { x:4, y:8 }, { x:2, y:9 }, { x:3, y:9 }, { x:4, y:9 },
+        { x:3, y:10 }
+      ]
+    },
+    {
+      color: "rgb(70, 70, 70)",
+      rbxs: [
+        { x:1, y:5 }, { x:0, y:6 }, { x:1, y:6 }, { x:1, y:7 }
+      ]
+    },
+    {
+      color: "rgb(90, 90, 90)",
+      rbxs: [
+        { x:5, y:5 }, { x:5, y:6 }, { x:6, y:6 }, { x:5, y:7 }
+      ]
+    },
+    {
+      color: "rgb(150, 150, 150)",
+      rbxs: [
+        { x:2, y:8 }, { x:3, y:8 }, { x:4, y:8 }, { x:3, y:9 }
+      ]
+    },
+    {
+      color: "rgb(160, 160, 160)",
+      rbxs: [
+        { x:2, y:5 }, { x:3, y:5 }, { x:4, y:5 }, { x:2, y:6 },
+        { x:4, y:6 }, { x:2, y:7 }, { x:4, y:7 }
+      ]
+    },
+    {
+      color: "rgb(170, 170, 170)",
+      rbxs: [
+        { x:2, y:1 }, { x:4, y:1 }, { x:2, y:2 }, { x:4, y:2 },
+        { x:2, y:3 }, { x:4, y:3 }, { x:2, y:4 }, { x:3, y:4 },
+        { x:4, y:4 }, { x:3, y:5 }
+      ]
+    }
+  ]
+}
+
+const rbxInfoSample00 = {
+  list: [frontSample00, frontSample01, frontSample02, frontSample03]
+}
