14d3355ffeba4fce41938be72349772860e091ea
zmdr0010 <89996377+zmdr0010@users.noreply.github.com>
2024-03-22 13:23:30 +0900

    Add files via upload

--- t-raw-expand/lib-drw-raw-layer.js
+++ t-raw-expand/lib-drw-raw-layer.js
@@ -54,3 +54,73 @@ function compRawInfoLayers(list) {
   }
   return result
 }
+
+function parsingRawInfoToLayerRawInfo(rawInfo) {
+  const layerList = divideRawLayer(rawInfo.raw, rawInfo.rawNum, rawInfo.row, rawInfo.column)
+  const layers = []
+  for (const layer of layerList) {
+    let digitList = []
+    for (const rw of layer) {
+      if (rw > 0) {
+        digitList.push(1)
+      } else {
+        digitList.push(0)
+      }
+    }
+    const jStr = digitList.join('')
+    // const num = parseInt(jStr, 2)
+    const num = BigInt(`0b` + jStr)
+    layers.push(num)
+  }
+  return {
+    uCode: `layer-raw-info-${rawInfo.uCode}`,
+    row: rawInfo.row,
+    column: rawInfo.column,
+    rawNum: rawInfo.rawNum,
+    layers: layers
+  }
+}
+
+function parsingLayerRawInfoToRawInfo(layerRawInfo) {
+  const length = layerRawInfo.row * layerRawInfo.column
+  const compRaw = []
+  for (let i=0; i<layerRawInfo.layers.length; i++) {
+    // let layerNum = 0
+    let layerNum = layerRawInfo.layers[i]
+    // console.log(Number(layerNum))
+    // const str = layerNum.toString(2)
+    // const big = BigInt(`${layerNum}`)
+    const big = BigInt(layerNum)
+    const str = big.toString(2)
+    const d = length - str.length
+    let jStr = str
+    if (d > 0) {
+      let add = ''
+      for (let i=0; i<d; i++) {
+        add += '0'
+      }
+      jStr = add.concat(str)
+      // console.log(`jStr : ${jStr.length}, length : ${length}, d : ${d}, add : ${add}`)
+    }
+    let rawStrList = jStr.split('')
+    // console.log(`rawStrList : ${rawStrList.length}`)
+    // let raw = []
+    for (let j=0; j<rawStrList.length; j++) {
+      const strRw = Number(rawStrList[j]) * i
+      // console.log(`i : ${i}, j : ${j}, strRw : ${strRw}`)
+      // raw.push(strRw)
+      if (compRaw.length-1 < j) {
+        compRaw.push(strRw)
+      } else {
+        compRaw[j] = Math.max(compRaw[j], strRw)
+      }
+    }
+  }
+  return {
+    uCode: `comp-${layerRawInfo.uCode}`,
+    row: layerRawInfo.row,
+    column: layerRawInfo.column,
+    rawNum: layerRawInfo.rawNum,
+    raw: compRaw
+  }
+}
